45b1cad3f754d8f8533956687016be5119d786f1
Simon Fagerli <fagerli93@live.no>
2021-11-02 15:06:20 +0100

    Added firebase config

--- app/frontend/App.jsx
+++ app/frontend/App.jsx
@@ -1,24 +1,25 @@
-import React from 'react';
-import 'babel-polyfill';
-import { polyfill } from 'es6-promise';
+import React from "react";
+import "babel-polyfill";
+import { polyfill } from "es6-promise";
+// import useFirebase  from './utils/firebaseConfig';
 polyfill();
 
-import { HashRouter, Route, Switch } from 'react-router-dom';
-import Hovedside from './Hovedside.jsx';
-import LeggTilBevis from './LeggTilBevis.jsx';
+import { HashRouter, Route, Switch } from "react-router-dom";
+import Hovedside from "./Hovedside.jsx";
+import LeggTilBevis from "./LeggTilBevis.jsx";
 
 class App extends React.Component {
-  render() {
-    return (
-    <HashRouter>
-      <Switch>
-        <Route exact path="/" component={Hovedside} />
-        <Route exact path="/nytt-bevis" component={LeggTilBevis} />
-      </Switch>
-
-    </HashRouter>
-    );
-  }
-};
+    // useFirebase();
+    render() {
+        return (
+            <HashRouter>
+                <Switch>
+                    <Route exact path='/' component={Hovedside} />
+                    <Route exact path='/nytt-bevis' component={LeggTilBevis} />
+                </Switch>
+            </HashRouter>
+        );
+    }
+}
 
 export default App;
