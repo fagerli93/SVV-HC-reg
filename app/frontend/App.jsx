dfa5bcd2cf6cc4d330951b96db9c2daf5edd8c40
Simon Fagerli <fagerli93@live.no>
2021-11-02 15:20:10 +0100

    Moving to functional components

--- app/frontend/App.jsx
+++ app/frontend/App.jsx
@@ -1,24 +1,21 @@
-import React from 'react';
-import 'babel-polyfill';
-import { polyfill } from 'es6-promise';
+import React from "react";
+import "babel-polyfill";
+import { polyfill } from "es6-promise";
 polyfill();
 
-import { HashRouter, Route, Switch } from 'react-router-dom';
-import Hovedside from './Hovedside.jsx';
-import LeggTilBevis from './LeggTilBevis.jsx';
+import { HashRouter, Route, Switch } from "react-router-dom";
+import Hovedside from "./Hovedside.jsx";
+import LeggTilBevis from "./LeggTilBevis.jsx";
 
-class App extends React.Component {
-  render() {
+const App = () => {
     return (
-    <HashRouter>
-      <Switch>
-        <Route exact path="/" component={Hovedside} />
-        <Route exact path="/nytt-bevis" component={LeggTilBevis} />
-      </Switch>
-
-    </HashRouter>
+        <HashRouter>
+            <Switch>
+                <Route exact path='/' component={Hovedside} />
+                <Route exact path='/nytt-bevis' component={LeggTilBevis} />
+            </Switch>
+        </HashRouter>
     );
-  }
 };
 
 export default App;
